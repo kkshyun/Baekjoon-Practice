a6aca1ca932e8a636f4ba88be599d0e37cff61fb
kkshyun <163789341+kkshyun@users.noreply.github.com>
2024-09-16 02:21:24 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2380 ms, Memory: 863836 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Deque<int[]> deque = new ArrayDeque<>();
+        Deque<Node> deque = new LinkedList<>();
         String[] str = br.readLine().split(" ");
         int N = Integer.parseInt(str[0]);
         int L = Integer.parseInt(str[1]);
@@ -16,40 +16,30 @@ public class Main {
             numArr[i] = Integer.parseInt(str[i]);
         }
 
-        deque.addLast(new int[] {0, numArr[0]});
-        bw.write(numArr[0] + " ");
-
-
-        for(int i = 1 ; i < N ; i++) {
-            while (!deque.isEmpty()) {
-                // 넣으려는 수보다 마지막에 존재하는 값이 크거나 같을 때 마지막 제거
-                if(deque.getLast()[1] >= numArr[i]){
-                    deque.removeLast();
-                }
-                // 넣으려는 수보다 마지막에 존재하는 값이 작을 때
-                else {
-                    break;
-                }
+        for(int i = 0 ; i < N ; i++) {
+            while (!deque.isEmpty() && deque.getLast().value >= numArr[i]) {
+                deque.removeLast();
             }
-            deque.addLast(new int[] {i, numArr[i]});
+            deque.addLast(new Node(numArr[i], i));
 
-//            System.out.println("원소");
-//            for(List l : deque) {
-//                System.out.print(l);
-//            }
-
-            // 윈도우 크기 : L 비교
-            // 만약 L이 3이고 i 10이면 8 9 10까지만 오케이
-
-
-            while(deque.getFirst()[0]<=i-L) {
+            while(deque.getFirst().index<=i-L) {
                 deque.removeFirst();
             }
 
-            bw.write(deque.getFirst()[1] + " ");
-//            System.out.println("result"+result[i]);
+            bw.write(deque.getFirst().value + " ");
         }
+        
         bw.flush();
         bw.close();
+    }
+    
+    static class Node {
+        public int value;
+        public int index;
+
+        public Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
     }
 }
