b840a85cd50288b641484cc3873748c557ace857
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2025-09-19 02:46:19 +0900

    [Silver III] Title: 프린터 큐, Time: 140 ms, Memory: 16480 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,44 +1,42 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            // 1 : 중요도, 0 : 순서
-            Queue<int[]> queue = new LinkedList<>();
-            String[] s = br.readLine().split(" ");
+        int Test = Integer.parseInt(br.readLine());
+        Queue<Integer> priorityQueue = new PriorityQueue<>((o1,o2) -> Integer.compare(o2,o1));
+        Queue<int[]> queue = new LinkedList<>();
+        String[] s;
+        for (int i = 0; i < Test; i++) {
+            priorityQueue.clear();
+            queue.clear();
+            s = br.readLine().split(" ");
             int N = Integer.parseInt(s[0]);
             int M = Integer.parseInt(s[1]);
-            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1,o2)->Integer.compare(o2,o1));
             s = br.readLine().split(" ");
             for (int j = 0; j < N; j++) {
-                queue.add(new int[]{j,Integer.parseInt(s[j])});
-                priorityQueue.add(Integer.parseInt(s[j]));
+                int num = Integer.parseInt(s[j]);
+                queue.add(new int[]{j,num});
+                priorityQueue.add(num);
             }
             int count = 0;
-            int find = 0;
-            while(!priorityQueue.isEmpty()) {
-                Integer poll = priorityQueue.poll();
-                while(!queue.isEmpty()) {
-                    int[] poll1 = queue.poll();
-                    if(poll != poll1[1]) {
-                        queue.add(poll1);
-                        continue;
-                    }
-                    count++;
-                    if(poll1[0] == M) {
-                        bw.write(count+"\n");
-                        find = 1;
-                    }
-                    break;
+            while(!queue.isEmpty() && !priorityQueue.isEmpty()) {
+                int priority = queue.peek()[1];
+                if(priorityQueue.peek() > priority) {
+                    queue.add(queue.poll());
+                    continue;
                 }
-                if(find==1)
+                int index = queue.poll()[0];
+                count++;
+                priorityQueue.poll();
+                if(index == M)
                     break;
             }
+            bw.write(count+"\n");
         }
         bw.flush();
         bw.close();
