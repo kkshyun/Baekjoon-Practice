86d968193dea6a479a9fc10a941f5c1d63cb625a
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2025-09-19 01:12:24 +0900

    [Silver IV] Title: 스택, Time: 188 ms, Memory: 19888 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,35 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.io.*;
 
 public class Main {
+    public static int[] stack;
+    public static int top = 0;
     public static void main(String[] args) throws IOException {
-        Stack<Integer> stack = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.readLine());
-        for (int i = 0; i < num; i++) {
-            String[] str = br.readLine().split(" ");
-            if(str[0].equals("push")) {
-                stack.push(Integer.parseInt(str[1]));
-            }else if(str[0].equals("pop")) {
-
-                if(stack.isEmpty()) System.out.println(-1);
-                else System.out.println(stack.pop());
-
-            }else if(str[0].equals("size")) {
-                System.out.println(stack.size());
-
-            }else if(str[0].equals("empty")) {
-
-                if(stack.isEmpty()) System.out.println("1");
-                else System.out.println("0");
-
-            }else if(str[0].equals("top")) {
-                if(stack.isEmpty()) System.out.println("-1");
-                else System.out.println(stack.peek());
-            }
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        String[] s;
+        int commandResult;
+        stack = new int[N+1];
+        for (int i = 0; i < N; i++) {
+            s = br.readLine().split(" ");
+            commandResult = stack(s);
+            if(s[0].equals("push"))
+                continue;
+            bw.write(commandResult+"\n");
         }
+        bw.flush();
+        bw.close();
+    }
+    public static int stack(String[] str) {
+        int stackResult = 0;
+        switch(str[0]) {
+            case "push" :
+                stack[++top] = Integer.parseInt(str[1]);
+                break;
+            case "pop" :
+                if(top==0) return -1;
+                int topNum = stack[top];
+                top--;
+                stackResult = topNum;
+                break;
+            case "size" :
+                stackResult = top;
+                break;
+            case "empty" :
+                stackResult = (top==0 ? 1 : 0);
+                break;
+            case "top" :
+                if(top==0) return -1;
+                stackResult = stack[top];
+                break;
+        }
+        return stackResult;
     }
 }
