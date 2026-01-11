286dc52efd48344ea758c312734657fe46a6eef0
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-11 23:59:20 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 18012 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,41 +1,31 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    static int count = 0;
     static int N, M;
-
+    static int[] arr;
     static BufferedWriter bw;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] s = br.readLine().split(" ");
-        N = Integer.parseInt(s[0]);
-        M = Integer.parseInt(s[1]);
-
-        for (int i = 1; i <= N ; i++) {
-            List result = new ArrayList<Integer>();
-            check(i,0,result);
-        }
+        String[] str = br.readLine().split(" ");
+        N = Integer.parseInt(str[0]);
+        M = Integer.parseInt(str[1]);
+        arr = new int[M];
+        combination(1,0);
         bw.flush();
         bw.close();
-
     }
-    public static void check(int v, int depth, List<Integer> result) throws IOException {
-        depth++;
-        result.add(v);
-        if (depth == M) {
-            for (int i = 0; i < result.size(); i++) {
-                bw.write(result.get(i) + " ");
+    public static void combination(int start, int r) throws IOException {
+        if(r == M) {
+            for (int i = 0; i < M; i++) {
+                bw.write(arr[i]+" ");
             }
             bw.write("\n");
-            result.remove(result.size()-1);
             return;
         }
-        for (int i = v; i <= N; i++) {
-            check(i, depth, result);
+        for (int i = start; i <= N; i++) {
+            arr[r] = i;
+            combination(i, r+1);
         }
-        result.remove(result.size()-1);
     }
 }
