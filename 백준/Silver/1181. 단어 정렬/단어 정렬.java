45afb4266f62d6c8f1446c0384971beacd3fe6d2
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-13 11:17:59 +0900

    [Silver V] Title: 단어 정렬, Time: 328 ms, Memory: 22256 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,34 +1,25 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        HashSet<String> hashSet = new HashSet<>();
-
-        for (int i = 0; i < N; i++) {
-            hashSet.add(br.readLine());
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i <N; i++) {
+            set.add(br.readLine());
         }
-        String[] arr = new String[hashSet.size()];
-        int i = 0;
-        for(String str:hashSet) {
-            arr[i] = str;
-            i++;
-        }
-
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                if(o1.length() == o2.length())
-                    return o1.compareTo(o2);
-                else
-                    return o1.length() - o2.length();
-            }
+        String[] arr = set.toArray(new String[0]);
+        Arrays.sort(arr, (s1, s2)-> {
+            if(s1.length() == s2.length())
+                return s1.compareTo(s2);
+            return s1.length() - s2.length();
         });
-        for (int j = 0; j < arr.length; j++) {
-            bw.write(arr[j]+"\n");
+        for (int i = 0; i < arr.length; i++) {
+            bw.write(arr[i]+"\n");
         }
         bw.flush();
         bw.close();
