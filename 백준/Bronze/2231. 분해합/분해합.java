616860d2b1c0d9828f7418810d128b2a7193e323
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-15 15:36:18 +0900

    [Bronze II] Title: 분해합, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,20 +1,25 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= 1000000; i++) {
-            String s = String.valueOf(i);
-            int sum = i;
-            for (int j = 0; j < s.length(); j++) {
-                sum += s.charAt(j)-'0';
-            }
-            if(sum == N) {
+        for (int i = 1; i < N; i++) {
+            if(cal(i) == N) {
                 System.out.println(i);
                 return;
             }
         }
         System.out.println(0);
+    }
+    public static int cal(int i) {
+        int sum = i;
+        while(i != 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+        return sum;
     }
 }
