0b1b17ff08430409a62d00b9460279b712912525
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-28 02:00:23 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[] st = br.readLine().split(" ");
-        int[] arr = new int[n];
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st[i]);
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if(arr[i]>arr[j]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
-                }
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        int[] dp = new int[N+1]; // 해당 인덱스를 포함하는 가장 긴 부분 수열
         int max = 0;
-        for (int a:dp) {
-            max = Math.max(max,a);
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 무조건 포함해야 업데이트
+                if(arr[j] < arr[i])
+                    dp[i] = Math.max(dp[j]+1, dp[i]);
+            }
+            max = Math.max(max, dp[i]);
         }
         System.out.println(max);
     }
