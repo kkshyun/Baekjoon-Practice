0f7d487850b21db7642b840fc271baf4f65bba9a
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-28 01:38:21 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18752 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,24 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[N]=0;
-        for (int i = N; i > 1; i--) {
-            if(dp[i]==Integer.MAX_VALUE) continue;
-            dp[i-1] = Math.min(dp[i-1],dp[i]+1);
-            if(i % 2 == 0) {
-                dp[i/2] = Math.min(dp[i/2],dp[i]+1);
-            }
-            if(i % 3 == 0) {
-                dp[i/3] = Math.min(dp[i/3],dp[i]+1);
-            }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] result = new int[N+1];
+        for (int i = 2; i < result.length; i++) {
+            if(i % 6 == 0) {
+                result[i] = Math.min(result[i/3], result[i/2]) + 1;
+            } else if(i % 3 == 0)
+                result[i] = Math.min(result[i/3], result[i-1]) + 1;
+            else if(i % 2 == 0)
+                result[i] = Math.min(result[i/2], result[i-1]) + 1;
+            else
+                result[i] = result[i-1] + 1;
         }
-        bw.write(dp[1]+"\n");
-        bw.flush();
-        bw.close();
+        System.out.println(result[N]);
     }
 }
