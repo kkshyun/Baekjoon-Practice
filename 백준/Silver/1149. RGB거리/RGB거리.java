a0cfcf564e87399f7cc0fbe8f09fa7595713c28c
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-28 11:16:26 +0900

    [Silver I] Title: RGB거리, Time: 104 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,41 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] colors;
-    static int[][] remember;
-    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        colors = new int[N][3];
-        remember = new int[N][3];
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split(" ");
-            colors[i][0] = Integer.parseInt(s[0]);
-            colors[i][1] = Integer.parseInt(s[1]);
-            colors[i][2] = Integer.parseInt(s[2]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[N+1][3];
+        int[][] arr = new int[N+1][3];
+        for (int i = 1; i < N+1; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        dp();
+        for (int i = 1; i < N+1; i++) {
+            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2]) + arr[i][0];
+            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2]) + arr[i][1];
+            dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1]) + arr[i][2];
+        }
+
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < 3; i++) {
-            if(remember[N-1][i]<min)
-                min = remember[N-1][i];
+            min = Math.min(min, dp[N][i]);
         }
         System.out.println(min);
-    }
-    public static void dp() {
-        remember[0][0] = colors[0][0];
-        remember[0][1] = colors[0][1];
-        remember[0][2] = colors[0][2];
-
-        for (int i = 1; i < N; i++) {
-            remember[i][0] = Math.min(remember[i-1][1] + colors[i][0], remember[i-1][2] + colors[i][0]);
-            remember[i][1] = Math.min(remember[i-1][0] + colors[i][1], remember[i-1][2] + colors[i][1]);
-            remember[i][2] = Math.min(remember[i-1][0] + colors[i][2], remember[i-1][1] + colors[i][2]);
-        }
     }
 }
