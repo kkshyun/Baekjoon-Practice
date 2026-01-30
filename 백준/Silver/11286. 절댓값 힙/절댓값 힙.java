64d5bb3a91e9142f62be14938f01c8efe5c10212
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-30 10:40:22 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25796 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,30 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1,o2)-> {
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-            if(first_abs == second_abs)
-                return o1 > o2 ? 1 : -1; // 절댓값이 같으면 음수 우선 정렬하기
-            else
-                return first_abs - second_abs;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2)->{
+            if(Math.abs(o1) == Math.abs(o2))
+                return o1 - o2;
+            return Math.abs(o1) - Math.abs(o2);
         });
-
-        for (int i = 0; i < n; i++) {
-            int request = Integer.parseInt(br.readLine());
-            if(request == 0) {
-                if(queue.isEmpty()) {
-                    System.out.println("0");
-                } else {
-                    System.out.println(queue.poll());
-                }
-            } else {
-                queue.add(request);
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            if(temp == 0) {
+                if(pq.isEmpty())
+                    sb.append("0\n");
+                else
+                    sb.append(pq.poll()).append("\n");
             }
+            else pq.add(temp);
         }
+        System.out.println(sb);
     }
 }
