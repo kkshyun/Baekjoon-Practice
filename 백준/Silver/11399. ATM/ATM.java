4230f54f3117a417af03437885b8ab5bd804a1ff
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-01-31 21:36:46 +0900

    [Silver IV] Title: ATM, Time: 108 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,38 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        String[] str = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(str[i]);
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N+1];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i < n; i++) {
-            int target = arr[i];
-            int index = i;
-            for (int j = 0; j < i; j++) {
-                if(target<arr[j]) {
-                    index = j;
-                    break;
-                }
-            }
-            for(int j = i-1 ; j >= index ;j--) {
-                arr[j+1]=arr[j];
-            }
-            arr[index] = target;
+        Arrays.sort(arr);
+        int[] S = new int[N+1];
+        int result = 0;
+        // 합 배열 만들기
+        for (int i = 1; i < S.length; i++) {
+            S[i] = arr[i] + S[i-1];
+            result += S[i];
         }
-
-        int[] S = new int[n];
-        S[0] = arr[0];
-        int sum = S[0];
-        for (int i = 1; i < n; i++) {
-            S[i] = S[i-1] + arr[i];
-            sum += S[i];
-        }
-        System.out.println(sum);
+        System.out.println(result);
     }
 }
