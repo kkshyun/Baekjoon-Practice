5c542ee06b197243217fe634bac637be57697b35
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-02-09 00:50:13 +0900

    [Gold V] Title: 거의 소수, Time: 248 ms, Memory: 29596 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -1,49 +1,35 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-        long a = Long.parseLong(s[0]);
-        long b = Long.parseLong(s[1]);
-        int sqrtB = (int) Math.sqrt(b);
-        boolean[] arr = new boolean[sqrtB+1];
-
-        for (int i = 2; i < arr.length; i++) {
-            arr[i] = true;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        boolean[] arr = new boolean[10000001];
+        Arrays.fill(arr, true);
+        arr[0] = false;
+        arr[1] = false;
+        for (int i = 2; i <= Math.sqrt(arr.length-1); i++) {
+            if(!arr[i])
+                continue;
+            for (int j = i+i; j < arr.length; j+=i) {
+                arr[j] = false;
+            }
         }
-
-        // 1. 소수 구하기
+        int count = 0;
         for (int i = 2; i < arr.length; i++) {
             if(!arr[i])
                 continue;
-//            for (int j = i+i; j < arr.length; j+=i) {
-//                if(j<arr.length)
-//                    arr[j] = false;
-//            }
-            int j = i+i;
-            while(j<arr.length) {
-                arr[j] = false;
-                j+=i;
-            }
-        }
-
-        // 2. 거의 소수 구하기
-        // b의 제곱근 이하만 답이 될 가능성이 있기 때문에 반복문을 이렇게 씀
-        int count = 0;
-        for (int i = 2; i < arr.length; i++) {
-            if(arr[i]) {
-//                for (long j = i*i; j <= b ; j*=i) {
-//                    if(j>=a)
-//                        count++;
-//                }
-                long j = (long)i*i;
-                while(j<=b) {
-                    if(j>=a)
-                        count++;
-                    if (j > b / i) break;
-                    j*=i;
-                }
+            long temp = i;
+            while(i <= (double)B/temp) {
+                if(i >= (double)A/temp)
+                    count++;
+                temp *= i;
             }
         }
         System.out.println(count);
