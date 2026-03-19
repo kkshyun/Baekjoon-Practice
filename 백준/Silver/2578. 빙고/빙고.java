dcc9674d87f99d0a059cd2c8323b83503ab78a41
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-03-19 16:10:47 +0900

    [Silver IV] Title: 빙고, Time: 100 ms, Memory: 14004 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,84 +1,94 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+class Main {
     static int[][] arr;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         arr = new int[5][5];
-        int[][] find = new int[5][5];
         StringTokenizer st;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5 ; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < 5 ; j++ ) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 0; i < 5; i++) {
+        boolean check = false;
+        for (int i = 0; i < 5 ; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 5; j++) {
-                int num = Integer.parseInt(st.nextToken());
-                find[i][j] = num;
-            }
-        }
-
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                for (int k = 0; k < 5; k++) {
-                    for (int l = 0; l < 5; l++) {
-                        if(arr[k][l] == find[i][j])
-                            arr[k][l] = 0;
-                    }
-                }
-                if(check() >= 3) {
-                    System.out.println(i*5+j+1);
-                    return;
+            for (int j = 0; j < 5 ; j++ ) {
+                checkWhere(Integer.parseInt(st.nextToken()));
+                if(checkBingo()>=3) {
+                    System.out.print(i*5 + j + 1);
+                    check = true;
+                    break;
                 }
             }
+            if(check)
+                break;
         }
     }
 
-    public static int check() {
-        boolean check = true;
-        int count = 0;
-        // 가로 확인
-        for (int i = 0; i < 5; i++) {
-            check = true;
-            for (int j = 0; j < 5; j++) {
-                if (arr[i][j] != 0)
-                    check = false;
+    static void checkWhere(int num) {
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 0; j < 5 ; j++ ) {
+                if(arr[i][j] == num) {
+                    arr[i][j] = 0;
+                    return;
+                }
             }
-            if(check)
-                count++;
-        }
+        } 
+    }
 
-        // 가로 확인
-        for (int i = 0; i < 5; i++) {
+    static int checkBingo() {
+        int count = 0;
+        boolean check = true;
+        
+        for (int i = 0; i < 5 ; i++) {
             check = true;
-            for (int j = 0; j < 5; j++) {
-                if (arr[j][i] != 0)
+            for (int j = 0; j < 5 ; j++ ) {
+                if(arr[i][j] != 0) {
                     check = false;
+                    break;
+                }
+            }
+            if(check)
+                count++;
+        }  
+        
+        for (int i = 0; i < 5 ; i++) {
+            check = true;
+            for (int j = 0; j < 5 ; j++ ) {
+                if(arr[j][i] != 0) {
+                    check = false;
+                    break;
+                }
             }
             if(check)
                 count++;
         }
-        // 대각선 확인
+        
         check = true;
-        for (int i = 0; i < 5; i++) {
-            if (arr[i][i] != 0)
+        for (int i = 0; i < 5 ; i++) {
+            if(arr[i][i] != 0) {
                 check = false;
-        }
+                break;
+            }
+        }     
         if(check)
             count++;
+        
         check = true;
-        for (int i = 0; i < 5; i++) {
-            if (arr[i][4-i] != 0)
+        for (int i = 0; i < 5 ; i++) {
+            if(arr[i][4-i] != 0) {
                 check = false;
-        }
+                break;
+            }
+        }     
         if(check)
             count++;
+        
         return count;
     }
 }
