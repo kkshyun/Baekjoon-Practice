72a590d51892c70ee60f978df083c306c292d2d5
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-03-23 15:20:37 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 564 ms, Memory: 48428 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,42 +1,27 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
-import java.util.PriorityQueue;
 
-public class Main {
-    static class Node implements Comparable<Node> {
-        int x;
-        int y;
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-
-        @Override
-        public int compareTo(Node o) {
-            if(this.x < o.x)
-                return -1;
-            else if(this.x > o.x)
-                return 1;
-            else if(this.y > o.y)
-                return 1;
-            else
-                return -1;
-        }
-    }
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split(" ");
-            queue.add(new Node(Integer.parseInt(s[0]),Integer.parseInt(s[1])));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> {
+                if(o1[0]==o2[0]) return o1[1] - o2[1];
+                return o1[0] - o2[0];
+        });
+        StringTokenizer st;
+        for (int i = 0; i < N ;i++ ) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            pq.add(new int[]{x, y});
         }
-        while(!queue.isEmpty()) {
-            Node poll = queue.poll();
-            bw.write(poll.x+" "+poll.y+"\n");
+        while(!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            sb.append(temp[0]).append(" ").append(temp[1]).append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
 }
