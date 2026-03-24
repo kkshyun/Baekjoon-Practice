0554b4eddae9ef7a3a996a3ec59fc17d12086762
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-03-24 16:50:29 +0900

    [Silver II] Title: 통계학, Time: 700 ms, Memory: 62832 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,50 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Serializable;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
         long sum = 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int[] arr = new int[N];
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            sum += arr[i];
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            int num = Integer.parseInt(br.readLine());
+            sum += num;
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            map.put(num, map.getOrDefault(num,0)+1);
+            arr[i] = num;
         }
         Arrays.sort(arr);
-        sb.append(Math.round((float) sum /N)).append("\n");
+        sb.append(Math.round((double)sum / N)).append("\n");
+            
         sb.append(arr[N/2]).append("\n");
-        int max = map.get(arr[0]);
-
-        PriorityQueue<int[]>  pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1[1] == o2[1]) // 값
-                return o1[0] - o2[0]; // 키
-            return o2[1] - o1[1]; // 값
-        });
-
-        for(Integer key : map.keySet()){
-            pq.add(new int[]{key, map.get(key)});
+        
+        List<Integer> many = new ArrayList<>();
+        int manyMax = 0;
+        for (Integer key : map.keySet()) {
+            int value = map.get(key);
+            if(manyMax == value) {
+                many.add(key);
+            } else if (manyMax < value) {
+                manyMax = value;
+                many.clear();
+                many.add(key);
+            }
         }
-
-        int[] poll = pq.poll();
-        int result = poll[0];
-        int value = poll[1];
-        if(!pq.isEmpty()) {
-            poll = pq.poll();
-            if(poll[1] == value)
-                result = poll[0];
-        }
-        sb.append(result).append("\n");
-
-
-        long ans = arr[N-1]-arr[0];
-        sb.append(ans).append("\n");
-        System.out.println(sb);
+        Collections.sort(many);
+        if(many.size() >= 2)
+            sb.append(many.get(1)).append("\n");
+        else
+            sb.append(many.get(0)).append("\n");
+        sb.append(max-min);
+        
+        System.out.print(sb);
     }
 }
