03e1b2f241e57790b23d9b9bb2ac1389ffee047c
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-03-29 17:32:27 +0900

    [Silver IV] Title: 수 찾기, Time: 520 ms, Memory: 46596 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,41 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    static int[] arr;
-    static int target;
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        Set<String> set = new HashSet<>();
+        for(int i = 0 ; i < N ; i++) {
+            set.add(st.nextToken());
         }
-        Arrays.sort(arr);
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            target = Integer.parseInt(st.nextToken());
-            sb.append(binarySearch(0, N-1)).append("\n");
-        }
-        System.out.println(sb);
-    }
-
-    static int binarySearch(int start, int end) {
-        while(start <= end) {
-            int mid = start + (end-start)/2;
-            if(arr[mid] < target)
-                start = mid + 1;
-            else if(arr[mid] > target)
-                end = mid - 1;
+        for(int i = 0 ; i < M ; i++) {
+            if(set.contains(st.nextToken()))
+                sb.append("1").append("\n");
             else
-                return 1;
+                sb.append("0").append("\n");
         }
-        return 0;
+        System.out.print(sb);
     }
 }
