cc198da3c86961751ae7d02859f3a1cc25c0b884
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-04-01 17:22:34 +0900

    [Silver II] Title: 유기농 배추, Time: 136 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,57 +1,59 @@
+import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-    static int result;
-    static int[][] arr;
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
-    static int N, M;
-    static BufferedWriter bw;
-    public static void main(String[] args) throws IOException {
+class Main {
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static boolean[][] graph;
+    static int M, N;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            String[] s = br.readLine().split(" ");
-            // 가로
-            M = Integer.parseInt(s[0]);
-            // 세로
-            N = Integer.parseInt(s[1]);
-            int K = Integer.parseInt(s[2]);
-            arr = new int[N][M];
+        
+        for(int i = 0 ; i < T ; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            graph = new boolean[M][N];
             for (int j = 0; j < K; j++) {
-                s = br.readLine().split(" ");
-                int x = Integer.parseInt(s[0]);
-                int y = Integer.parseInt(s[1]);
-                arr[y][x] = 1;
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                graph[x][y] = true;
             }
-            result = 0;
-
-            // j : 세로, k : 가로
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    if(arr[j][k]==1) {
-                        result+=1;
-                        DFS(j,k);
+            int count = 0;
+            for (int j = 0; j < M; j++) {
+                for (int l = 0 ; l < N ; l++) {
+                    if(graph[j][l]) {
+                        bfs(j, l);
+                        count++;
                     }
                 }
             }
-            bw.write(result+"\n");
+            sb.append(count).append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.print(sb);
     }
-    public static void DFS(int y, int x) throws IOException {
-        arr[y][x] = 0;
-        for (int i = 0; i < 4; i++) {
-            int moveX = x+dx[i];
-            int moveY = y+dy[i];
-            if(moveX>=0&&moveY>=0&&moveX<M&&moveY<N) {
-                if(arr[moveY][moveX]!=0)
-                    DFS(moveY,moveX);
+
+    static void bfs(int x, int y) {
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{x,y});
+        graph[x][y] = false;
+        while(!queue.isEmpty()) {
+            int[] temp = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = temp[0] + dx[i];
+                int ny = temp[1] + dy[i];
+                if(nx < M && ny < N && nx >= 0 && ny >= 0) {
+                    if(graph[nx][ny]) {
+                        graph[nx][ny] = false;
+                        queue.add(new int[]{nx, ny});
+                    }
+                }
             }
         }
-
     }
 }
