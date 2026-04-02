d2fbc2d6d53537ca227b10d6cf1606e80c040e83
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-04-02 18:11:09 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,48 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    static int[][] graph;
-    static boolean[][] visited;
-    static int N, M;
+class Main {
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    public static void main(String[] args) throws IOException {
+    static int N, M;
+    static int[][] graph;
+    static int[][] dist;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         graph = new int[N][M];
-        visited = new boolean[N][M];
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split("");
-            for (int j = 0; j < M; j++) {
-                graph[i][j] = Integer.parseInt(s[j]);
+        dist = new int[N][M];
+        for(int i = 0 ; i < N ; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M ; j++) {
+                graph[i][j] = str.charAt(j) - '0';
             }
         }
-        System.out.println(bfs());
+        System.out.print(bfs());
     }
     static int bfs() {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
-
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{0,0});
+        graph[0][0] = 0;
+        dist[0][0] = 1;
         while(!queue.isEmpty()) {
-            int[] poll = queue.poll();
-            if(poll[0] == N-1 && poll[1] == M-1) {
-                return poll[2];
-            }
+            int[] now = queue.poll();
+            int x = now[0];
+            int y = now[1];
+            if(x == N-1 && y == M-1)
+                return dist[x][y];
             for (int i = 0; i < 4; i++) {
-                int nextX = poll[0] + dx[i];
-                int nextY = poll[1] + dy[i];
-                if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M && graph[nextX][nextY] == 1) {
-                    if(!visited[nextX][nextY]) {
-                        visited[nextX][nextY] = true;
-                        queue.add(new int[]{nextX, nextY, poll[2]+1});
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(nx < N && ny < M && nx >= 0 && ny >=0) {
+                    if(graph[nx][ny] == 1) {
+                        graph[nx][ny] = 0;
+                        dist[nx][ny] = dist[x][y]+1;
+                        queue.add(new int[]{nx, ny});
                     }
                 }
             }
