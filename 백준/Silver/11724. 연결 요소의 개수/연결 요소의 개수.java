40fd6252d6126df27d553a64c421547a41a6000f
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-04-02 16:03:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 604 ms, Memory: 142212 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,46 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
-    static ArrayList<Integer>[] graph;
+class Main {
     static boolean[] visited;
-    public static void main(String[] args) throws IOException {
+    static ArrayList<Integer>[] graph;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[N];
-        visited = new boolean[N];
-        for (int i = 0; i < N; i++) {
-            graph[i] = new ArrayList<>();
+        graph = new ArrayList[N+1];
+        visited = new boolean[N+1];
+        for(int i = 0 ; i < N+1 ; i++) {
+            graph[i] = new ArrayList<Integer>();
         }
-        for (int i = 0; i < M; i++) {
+        for(int i = 0 ; i < M ; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()) - 1;
-            int b = Integer.parseInt(st.nextToken()) - 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             graph[a].add(b);
             graph[b].add(a);
         }
         int count = 0;
-        for (int i = 0; i < N; i++) {
+        for(int i = 1 ; i < N+1 ; i++) {
             if(!visited[i]) {
-                dfs(i);
                 count++;
+                dfs(i);
             }
         }
-        System.out.println(count);
+        System.out.print(count);
     }
 
-    public static void dfs(int start) {
-        if(visited[start])
-            return;
-        visited[start] = true;
-        for(int next : graph[start]) {
-            if(!visited[next])
+    public static void dfs(int n) {
+        visited[n] = true;
+        for(int next : graph[n]) {
+            if(!visited[next]) {
                 dfs(next);
+            }
         }
     }
 }
