4a75b15c8562f0551df2a369cf5a8e36b995dbdd
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-06-25 19:03:56 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,4 +1,5 @@
-SELECT USER_ID, NICKNAME, CONCAT(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) AS  '전체주소', CONCAT_WS('-',LEFT(TLNO,3),RIGHT(LEFT(TLNO,7),4),RIGHT(TLNO,4)) AS '전화번호'
-FROM USED_GOODS_USER U
-WHERE USER_ID IN (SELECT WRITER_ID FROM USED_GOODS_BOARD GROUP BY WRITER_ID HAVING COUNT(*) >= 3)
-ORDER BY 1 DESC;
+-- 코드를 입력하세요
+SELECT USER_ID, NICKNAME, concat(CITY,' ',STREET_ADDRESS1,' ',STREET_ADDRESS2) as '전체주소', concat(substring(TLNO,1,3),'-',substring(TLNO,4,4),'-',substring(TLNO,8,4))as '전화번호'
+from USED_GOODS_USER
+where user_id in (select writer_id from USED_GOODS_BOARD group by writer_id having count(*) >= 3)
+order by USER_ID desc
