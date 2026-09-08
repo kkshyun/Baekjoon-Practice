812490cd8f021ca0737494da5b2d0dd3c650a4b8
🐰SEHYUN🐰 <kkshyun56@gmail.com>
2026-09-08 13:33:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.31 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 95.6 MB, 시간: 163.47 ms
+메모리: 117 MB, 시간: 48.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 29일 12:54:01
+2026년 09월 08일 13:33:48
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,17 +2,16 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-
-        for(int i = 0;i<completion.length;i++) {
-            map.put(completion[i], map.getOrDefault(completion[i],0)+1);
+        for(String s : completion) {
+            map.put(s, map.getOrDefault(s,0)+1);
         }
-
-        for(int i = 0; i<participant.length;i++) {
-            if(map.getOrDefault(participant[i],0) == 0){
-                return participant[i];
+        for(String s : participant) {
+            if(map.containsKey(s) && map.get(s) > 0) {
+                map.put(s, map.get(s)-1);
+            } else {
+                return s;
             }
-            map.put(participant[i],map.getOrDefault(participant[i],0)-1);
         }
-        return null;
+        return "false";
     }
 }
